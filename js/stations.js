b3313840723712961ba2b35c2da3f1a4c41e0800
Rimfoer <polegoshkosergey@mail.ru>
2025-12-28 11:46:15 +0700

    Update stations.js
    fixed minor things

--- js/stations.js
+++ js/stations.js
@@ -1,5 +1,5 @@
 const animParams = {fps:30,animations:{0:[0],1:[0,1],r1:[1,0],2:[0,1,2],r2:[2,1,0],3:[0,1,2,3],r3:[3,2,1,0],4:[0,1,2,3,4],r4:[4,3,2,1,0],5:[0,1,2,3,4,5],r5:[5,4,3,2,1,0],6:[0,1,2,3,4,5,6],r6:[6,5,4,3,2,1,0],7:[0,1,2,3,4,5,6,7],r7:[7,6,5,4,3,2,1,0],8:[0,1,2,3,4,5,6,7,8],r8:[8,7,6,5,4,3,2,1,0],9:[0,1,2,3,4,5,6,7,8,9],r9:[9,8,7,6,5,4,3,2,1,0],10:[0,1,2,3,4,5,6,7,8,9,10],r10:[10,9,8,7,6,5,4,3,2,1,0]},loop: false,autoPlay: false},
-	clr = {"artist":{"off":"rgb(255,255,255,0)","on":"rgb(255,255,255,1)"},"title":{"off":"rgb(201,201,201,0)","on":"rgb(201,201,201,1)"}},
+	clr = {"artist":{"off":"rgb(255,255,255,0)","on":"rgb(255,255,255,1)"},"title":{"off":"rgb(163,163,163,0)","on":"rgb(163,163,163,1)"}},
 	parser = ['record','ps','tm','teo'],
 	showTitles = ['Record Club', 'Record Club Chart', 'Record News', 'Record Dance Radio', 'Record Superchart', 'Вейкаперы', 'Кремов и Хрусталёв', 'by DJ Peretse'],
 	stream = ['https://radiorecord.hostingradio.ru/rr_main96.aacp','https://radiorecord.hostingradio.ru/ps96.aacp','https://radiorecord.hostingradio.ru/tm96.aacp','https://radiorecord.hostingradio.ru/teo96.aacp'];
@@ -38,28 +38,25 @@ function currPlayerStatus(stat) {
 function openURL(windowName, url) {
 	if(!window.popups) window.popups = [];
 	let wnd = window.popups[windowName];
+	let resolution = url !== window.location.href ? 'width=839,height=420' : 'width=418,height=544';
 	if(wnd && !wnd.closed) wnd.focus(); 
-	wnd = window.open(url, windowName, "top=100,left=200,width=418,height=544,location=no,menubar=no,resizable=no,scrollbars=no,status=no,toolbar=no");
+	wnd = window.open(url, windowName, `top=100,left=200,${resolution},location=no,menubar=no,resizable=no,scrollbars=no,status=no,toolbar=no`);
 	wnd.focus();
 	window.popups[windowName] = wnd;
-}
-
-function showOnlinePlayer(tab) {
-	openURL($('head').children('title').html(), `${window.location.href}`);
 }
 
 function parseTitle(Titler, txt1, txt2) {
 	$.each(Titler, function(key, val) {
 		switch(key) {
 			case 'artist': {
-				txt1.html(val.setArtistName());
+				txt1.html(val.setArtistName(Titler.trackname));
 				txt1.animate({color: clr.artist.on}, 400, function() {
 					$(this).attr('title', val);
 				});
 				break;
 			}
 			case 'trackname': {
-				txt2.html(val.setSongName());
+				txt2.html(val.setSongName(Titler.artist));
 				txt2.animate({color: clr.title.on}, 400, function() {
 					$(this).attr('title', val);
 					currParser>=3 ? currParser = 0 : currParser++;
@@ -94,31 +91,15 @@ function hideTextMetadata(num) {
 }
 
 Number.prototype.limiter = function(num) {
-	if(Number(this) > 100) {
-		return 100;
-	} else if(Number(this) < 0) {
-		return 0;
-	} else {
-		return Number(this);
-	}
+	return (Number(this) > 100 ? 100 : Number(this) < 0 ? 0 : Number(this));
 }
 
-String.prototype.setArtistName = function(a_name) {
-	if(String(this) === '' || String(this) === 'Radio Record') {
-		return 'В эфире:';
-	} else {
-		return String(this);
-	}
+String.prototype.setArtistName = function(s_name) {
+	return (String(this) === '' && showTitles.includes(s_name) || String(this) === 'Radio Record' && showTitles.includes(s_name) ? `В эфире: ${s_name}` :  String(this));
 };
 
-String.prototype.setSongName = function(s_name) {
-	if(String(this) === '') {
-		return ' ― ';
-	} else if(String(this).indexOf('#') !== -1 || String(this).indexOf('Record Superchart') !== -1 || showTitles.includes(String(this))) {
-		return String(this);
-	} else {
-		return `— ${String(this)}`;
-	}
+String.prototype.setSongName = function(a_name) {
+	return (String(this) === '' || showTitles.includes(String(this)) ? '' : specialChars.test(String(this)) ? String(this) : `— ${String(this)}`);
 };
 
 function setWatch(ts) {
@@ -217,6 +198,7 @@ function showMessage(txt) {
 			$('.watch').addClass('active');
 			$('.station.active').children('.volume').addClass('active');
 			$('.station.active').children('.station-text').addClass('shorted');
+			localStorage.playSource = 'current';
 		},
 		stop: function() {
 			soundManager.destroySound('record');
@@ -229,7 +211,9 @@ function showMessage(txt) {
 			currTime = 0,
 			playStatus = 'stopped';
 			setWatch(0);
+			localStorage.playSource = '';
 		}
 	});
 
 })(jQuery);
+
