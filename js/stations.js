04f4c5f37694cb02cdfb123530a26eb7d6592f7a
Rimfoer <polegoshkosergey@mail.ru>
2026-01-06 16:52:06 +0700

    Update stations.js
    добавлен метод stripWhitespace

--- js/stations.js
+++ js/stations.js
@@ -1,10 +1,10 @@
-const animParams = {fps:60,animations:{0:[0],1:[0,1],r1:[1,0],2:[0,1,2],r2:[2,1,0],3:[0,1,2,3],r3:[3,2,1,0],4:[0,1,2,3,4],r4:[4,3,2,1,0],5:[0,1,2,3,4,5],r5:[5,4,3,2,1,0],6:[0,1,2,3,4,5,6],r6:[6,5,4,3,2,1,0],7:[0,1,2,3,4,5,6,7],r7:[7,6,5,4,3,2,1,0],8:[0,1,2,3,4,5,6,7,8],r8:[8,7,6,5,4,3,2,1,0],9:[0,1,2,3,4,5,6,7,8,9],r9:[9,8,7,6,5,4,3,2,1,0],10:[0,1,2,3,4,5,6,7,8,9,10],r10:[10,9,8,7,6,5,4,3,2,1,0]},loop: false,autoPlay: false},
-	clr = {"artist":{"off":"rgb(255,255,255,0)","on":"rgb(255,255,255,1)"},"title":{"off":"rgb(163,163,163,0)","on":"rgb(163,163,163,1)"}},
-	parser = ['record','ps','tm','teo'],
-	showTitles = ['Record Club', 'Record Club Chart', 'Record News', 'Record Dance Radio', 'Record Superchart', 'Selection', 'Вейкаперы', 'Кремов и Хрусталёв', 'by DJ Peretse'],
-	stream = ['https://radiorecord.hostingradio.ru/rr_main96.aacp','https://radiorecord.hostingradio.ru/ps96.aacp','https://radiorecord.hostingradio.ru/tm96.aacp','https://radiorecord.hostingradio.ru/teo96.aacp'];
-	specialChars = /[@#$%^*_\=\[\]{};:"\\|<>\/]/;
-let getCurVolPos = 0;
+const animParams = {fps:60,animations:{0:[0],1:[0,1],r1:[1,0],2:[0,1,2],r2:[2,1,0],3:[0,1,2,3],r3:[3,2,1,0],4:[0,1,2,3,4],r4:[4,3,2,1,0],5:[0,1,2,3,4,5],r5:[5,4,3,2,1,0],6:[0,1,2,3,4,5,6],r6:[6,5,4,3,2,1,0],7:[0,1,2,3,4,5,6,7],r7:[7,6,5,4,3,2,1,0],8:[0,1,2,3,4,5,6,7,8],r8:[8,7,6,5,4,3,2,1,0],9:[0,1,2,3,4,5,6,7,8,9],r9:[9,8,7,6,5,4,3,2,1,0],10:[0,1,2,3,4,5,6,7,8,9,10],r10:[10,9,8,7,6,5,4,3,2,1,0]},loop: false,autoPlay: false};
+const clr = {"artist":{"off":"rgb(255,255,255,0)","on":"rgb(255,255,255,1)"},"title":{"off":"rgb(163,163,163,0)","on":"rgb(163,163,163,1)"}};
+const parser = ['record','ps','tm','teo'];
+const showArtists = ['Gvozd', 'Record Megamix', 'Selection', 'Вейкаперы', 'Кремов и Хрусталёв'];
+const showTitles = ['Record Club', 'Record News', 'Record Superchart', 'Record Club Chart', 'Record Dance Radio', 'by DJ Peretse'];
+const stream = ['https://radiorecord.hostingradio.ru/rr_main96.aacp','https://radiorecord.hostingradio.ru/ps96.aacp','https://radiorecord.hostingradio.ru/tm96.aacp','https://radiorecord.hostingradio.ru/teo96.aacp'];
+const specialChars = /[@#$%^*_\=\[\]{};:"\\|<>\/]/;
 
 function currPlayerStatus(stat) {
 	let cur = $('.station.active').find('.play-button');
@@ -46,19 +46,24 @@ function openURL(windowName, url) {
 	window.popups[windowName] = wnd;
 }
 
-function parseTitle(Titler, elem) {
+function parseTitle(Titler, txt1, txt2) {
 	$.each(Titler, function(key, val) {
 		switch(key) {
 			case 'artist': {
-				elem.find('.station-track-artist').html(val.setArtistName(Titler.trackname));
-				elem.find('.station-track-artist').animate({color: clr.artist.on}, 400, function() {
+				txt1.animate({color: clr.artist.off}, 400, function() {
+					$(this).html(val.setArtistName(Titler.trackname));
+				});
+				txt1.delay(150).animate({color: clr.artist.on}, 500, function() {
 					$(this).attr('title', val);
+					$('.station').children('.pie-timer').removeClass('active');
 				});
 				break;
 			}
 			case 'trackname': {
-				elem.find('.station-track-title').html(val.setSongName(Titler.artist));
-				elem.find('.station-track-title').animate({color: clr.title.on}, 400, function() {
+				txt2.animate({color: clr.title.off}, 400, function() {
+					$(this).html(val.setSongName(Titler.artist));
+				});
+				txt2.delay(150).animate({color: clr.title.on}, 500, function() {
 					$(this).attr('title', val);
 					currParser>=3 ? currParser = 0 : currParser++;
 					$(`.station:eq(${currParser})`).children('.pie-timer').addClass('active');
@@ -72,19 +77,16 @@ function parseTitle(Titler, elem) {
 
 function updateTitle(startWith) {
 	if(mInterval !== undefined) clearInterval(mInterval);
-	$('.station').children('.pie-timer').removeClass('active');
 	$.getJSON(`https://tags.radiorecord.fm/now.php?chan=${parser[startWith>=0 ? startWith : currParser]}`).done(function(data) {
-		setTimeout(() => hideTextMetadata($(`.station:eq(${currParser})`)), 9600);
-		parseTitle(data, $(`.station:eq(${currParser})`));
+		parseTitle(
+			data,
+			$(`.station:eq(${currParser})`).find('.station-track-artist'),
+			$(`.station:eq(${currParser})`).find('.station-track-title')
+		);
 	}).fail(function() {
 		showMessage('notloaded');
 		mInterval = setInterval(() => updateTitle(), 10000);
 	});
-}
-
-function hideTextMetadata(el) {
-	el.find('.station-track-artist').animate({color: clr.artist.off}, 400);
-	el.find('.station-track-title').animate({color: clr.title.off}, 400);
 }
 
 Number.prototype.limiter = function(num) {
@@ -92,12 +94,73 @@ Number.prototype.limiter = function(num) {
 }
 
 String.prototype.setArtistName = function(s_name) {
-	return (String(this) === '' && showTitles.includes(s_name) || String(this) === 'Radio Record' && showTitles.includes(s_name) ? `В эфире: ${s_name}` : String(this) === '' ? 'В эфире:' :  String(this));
+	if(String(this) === '' && showTitles.includes(s_name) || String(this) === 'Radio Record' && showTitles.includes(s_name)) {
+		return `В эфире: ${s_name}`;
+	} else if(showArtists.includes(String(this)) && showTitles.includes(s_name)) {
+		return `В эфире: ${String(this)} (${s_name})`;
+	} else if(String(this) === '') {
+		return 'В эфире:';
+	} else {
+		return String(this).stripWhitespace();
+	}
 };
 
 String.prototype.setSongName = function(a_name) {
-	return (String(this) === '' || showTitles.includes(String(this)) ? '' : specialChars.test(String(this)) ? String(this) : `— ${String(this)}`);
+	if(String(this) === '' || showTitles.includes(String(this))) {
+		return '';
+	} else if(specialChars.test(String(this))) {
+		return String(this).stripWhitespace();
+	} else {
+		return '— ' + String(this).stripWhitespace();
+	}
 };
+
+String.prototype.stripWhitespace = function(options) {
+	let txt_0 = this;
+	if(options === undefined || options.indexOf("t", 0) > -1) txt_0 = txt_0.split("\t").join(" ");
+	if(options === undefined || options.indexOf("r", 0) > -1) txt_0 = txt_0.split("\r").join(" ");
+	if(options === undefined || options.indexOf("n", 0) > -1) txt_0 = txt_0.split("\n").join(" ");
+	if(options === undefined || options.indexOf("s", 0) > -1) {
+		txt_0 = txt_0.split(" ");
+		for(idx in txt_0) {
+			if(txt_0[idx].length === 0) txt_0.splice(idx, 1);
+		}
+		txt_0 = txt_0.join(" ");
+	}
+	if(options === undefined || options.indexOf("&apos;", 0) > -1) txt_0 = txt_0.split("&apos;").join("\'");
+	if(options === undefined || options.indexOf("&apos;", 0) > -1) txt_0 = txt_0.split("&amp;").join("&");
+	if(txt_0.indexOf("<") >= 0 && txt_0.indexOf(">") >= 0) {
+		let txt_3 = new Array();
+		let txt_1 = 0;
+		while(txt_0.indexOf(">", txt_1) > txt_1) {
+			cut = txt_1,
+			txt_1 = txt_1 + 1;
+		}
+		txt_1 = cut + 2;
+		while(_loc2_.indexOf("<", _loc3_) > _loc3_) {
+			txt_3.push(txt_0.charAt(txt_1));
+			txt_1 = txt_1 + 1;
+		}
+		txt_0 = txt_3.join("").toString();
+	}
+	if(txt_0.indexOf("&lt;") >= 0 && txt_0.indexOf("&gt;") >= 0) {
+		txt_3 = new Array(),
+		txt_1 = 0;
+		while(txt_0.indexOf("&gt;", txt_1) > txt_1) {
+			cut = txt_1,
+			txt_1 = txt_1 + 1;
+		}
+		txt_1 = cut + 5;
+		while(txt_0.indexOf("&lt;", txt_1) > txt_1) {
+			txt_3.push(txt_0.charAt(txt_1));
+			txt_1 = txt_1 + 1;
+		}
+		txt_0 = txt_3.join("").toString();
+	}
+	if(txt_0.length >= 27) txt_0 = `${txt_0.slice(0,26)} .&nbsp.&nbsp.`;
+	if(txt_0) return txt_0;
+	return "";
+}
 
 function setWatch(ts) {
 	let time = Math.floor(ts/1000);
@@ -112,8 +175,8 @@ function setWatch(ts) {
 		case 'playing': {
 			$('.watch').children('.playing-time').html(`
 				<span style="color: #666666">${hours}</span>
-				<span style="color: #A6A6A6; margin-left: ${watch_pos}">:${minutes}</span>
-				<span style="color: rgb(255,255,255,1); margin-left: ${watch_pos}">:${seconds}</span>
+				<span style="color: #A6A6A6; margin-left: -4px">:${minutes}</span>
+				<span style="color: rgb(255,255,255,1); margin-left: -4px">:${seconds}</span>
 				<span style="color: #575757">&nbsp${milliseconds}</span>
 			`);
 			break;
@@ -121,8 +184,8 @@ function setWatch(ts) {
 		default: {
 			$('.watch').children('.playing-time').html(`
 				<span style="color: #666666">${hours}</span>
-				<span style="color: #A6A6A6; margin-left: ${watch_pos}">:${minutes}</span>
-				<span style="color: rgb(255,255,255,1); margin-left: ${watch_pos}">:${seconds}</span>
+				<span style="color: #A6A6A6; margin-left: -4px">:${minutes}</span>
+				<span style="color: rgb(255,255,255,1); margin-left: -4px">:${seconds}</span>
 			`);
 		}
 	}
@@ -199,7 +262,3 @@ function showMessage(txt) {
 	});
 
 })(jQuery);
-
-
-
-
