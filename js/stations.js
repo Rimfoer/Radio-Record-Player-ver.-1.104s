292a38040a0147265d56db449076396346a381ce
Rimfoer <polegoshkosergey@mail.ru>
2026-01-12 00:13:51 +0700

    Update stations.js

--- js/stations.js
+++ js/stations.js
@@ -1,10 +1,10 @@
 const animParams = {fps:60,animations:{0:[0],1:[0,1],r1:[1,0],2:[0,1,2],r2:[2,1,0],3:[0,1,2,3],r3:[3,2,1,0],4:[0,1,2,3,4],r4:[4,3,2,1,0],5:[0,1,2,3,4,5],r5:[5,4,3,2,1,0],6:[0,1,2,3,4,5,6],r6:[6,5,4,3,2,1,0],7:[0,1,2,3,4,5,6,7],r7:[7,6,5,4,3,2,1,0],8:[0,1,2,3,4,5,6,7,8],r8:[8,7,6,5,4,3,2,1,0],9:[0,1,2,3,4,5,6,7,8,9],r9:[9,8,7,6,5,4,3,2,1,0],10:[0,1,2,3,4,5,6,7,8,9,10],r10:[10,9,8,7,6,5,4,3,2,1,0]},loop: false,autoPlay: false};
-const clr = {"artist":{"off":"rgb(255,255,255,0)","on":"rgb(255,255,255,1)"},"title":{"off":"rgb(163,163,163,0)","on":"rgb(163,163,163,1)"}};
+const clr = {"artist":{"off":"rgb(255,255,255,0)","on":"rgb(255,255,255,.7)"},"title":{"off":"rgb(163,163,163,0)","on":"rgb(163,163,163,.8)"}};
 const parser = ['record','ps','tm','teo'];
-const showArtists = ['Gvozd', 'Record Megamix', 'Selection', 'Вейкаперы', 'Кремов и Хрусталёв'];
-const showTitles = ['Record Club', 'Record News', 'Record Superchart', 'Record Club Chart', 'Record Dance Radio', 'by DJ Peretse'];
+const showArtists = ['Gvozd', 'Record Megamix', 'Selection'];
+const showTitles = ['Record Club', 'Record News', 'Record Superchart', 'Record Club Chart', 'Record Dance Radio', 'by DJ Peretse', 'Вейкаперы', 'Кремов и Хрусталёв'];
 const stream = ['https://radiorecord.hostingradio.ru/rr_main96.aacp','https://radiorecord.hostingradio.ru/ps96.aacp','https://radiorecord.hostingradio.ru/tm96.aacp','https://radiorecord.hostingradio.ru/teo96.aacp'];
-const specialChars = /[@#$%^*_\=\{};:"\\|<>\/]/;
+const specialChars = /[@#$^*_\=\{};:"\\|<>\/]/;
 
 function currPlayerStatus(stat) {
 	let cur = $('.station.active').find('.play-button');
@@ -39,7 +39,7 @@ function currPlayerStatus(stat) {
 function openURL(windowName, url) {
 	if(!window.popups) window.popups = [];
 	let wnd = window.popups[windowName];
-	let resolution = url !== window.location.href ? 'width=839,height=420' : 'width=422,height=554';
+	let resolution = url !== window.location.href ? 'width=844,height=422' : 'width=422,height=554';
 	if(wnd && !wnd.closed) wnd.focus(); 
 	wnd = window.open(url, windowName, `top=100,left=200,${resolution},location=no,menubar=no,resizable=no,scrollbars=no,status=no,toolbar=no`);
 	wnd.focus();
@@ -52,22 +52,21 @@ function parseTitle(Titler, txt1, txt2) {
 			case 'artist': {
 				txt1.animate({color: clr.artist.off}, 400, function() {
 					$(this).html(val.setArtistName(Titler.trackname));
-				});
-				txt1.delay(150).animate({color: clr.artist.on}, 500, function() {
 					$(this).attr('title', val);
-					$('.station').children('.pie-timer').removeClass('active');
+					txt1.delay(150).animate({color: clr.artist.on}, 400);
 				});
 				break;
 			}
 			case 'trackname': {
 				txt2.animate({color: clr.title.off}, 400, function() {
 					$(this).html(val.setSongName(Titler.artist));
-				});
-				txt2.delay(150).animate({color: clr.title.on}, 500, function() {
 					$(this).attr('title', val);
-					currParser>=3 ? currParser = 0 : currParser++;
-					$(`.station:eq(${currParser})`).children('.pie-timer').addClass('active');
-					mInterval = setInterval(() => updateTitle(), 10000);
+					$('.station').children('.pie-timer').removeClass('active');
+					txt2.delay(150).animate({color: clr.title.on}, 400, function() {
+						currParser>=3 ? currParser = 0 : currParser++;
+						$(`.station:eq(${currParser})`).children('.pie-timer').addClass('active');
+						mInterval = setInterval(() => updateTitle(), 10000);
+					});
 				});
 				break;
 			}
@@ -84,9 +83,13 @@ function updateTitle(startWith) {
 			$(`.station:eq(${currParser})`).find('.station-track-title')
 		);
 	}).fail(function() {
+		$('.station').children('.pie-timer').removeClass('active');
 		showMessage('notloaded');
+		currParser>=3 ? currParser = 0 : currParser++
+		$(`.station:eq(${currParser})`).delay(200).children('.pie-timer').addClass('active');
 		mInterval = setInterval(() => updateTitle(), 10000);
 	});
+	return false;
 }
 
 Number.prototype.limiter = function(num) {
@@ -94,72 +97,48 @@ Number.prototype.limiter = function(num) {
 }
 
 String.prototype.setArtistName = function(s_name) {
-	if(String(this) === '' && showTitles.includes(s_name) || String(this) === 'Radio Record' && showTitles.includes(s_name)) {
+	let artist = String(this);
+	if(['Record','Radio Record',''].includes(artist) && checkShowIncludes(s_name)) {
 		return `В эфире: ${s_name}`;
-	} else if(showArtists.includes(String(this)) && showTitles.includes(s_name)) {
-		return `В эфире: ${String(this)} (${s_name})`;
-	} else if(String(this) === '') {
+	} else if(showArtists.includes(artist)) {
+		return `В эфире: ${artist}`;
+	} else if(['Record','Radio Record',''].includes(artist) && !s_name) {
 		return 'В эфире:';
 	} else {
-		return String(this).stripWhitespace();
+		return artist.stripWhitespace('artist');
 	}
 };
 
 String.prototype.setSongName = function(a_name) {
-	if(String(this) === '' || showTitles.includes(String(this))) {
-		return '';
-	} else if(specialChars.test(String(this))) {
-		return String(this).stripWhitespace();
+	let title = String(this);
+	if(title === "" || showTitles.includes(title) && !a_name) {
+		return '—';
+	} else if(showTitles.includes(title) && a_name !== "") {
+		return title;
+	} else if(specialChars.test(title)) {
+		return title.stripWhitespace('title0');
 	} else {
-		return '— ' + String(this).stripWhitespace();
+		return '— ' + title.stripWhitespace('title1');
 	}
 };
 
-String.prototype.stripWhitespace = function(options) {
-	let txt_0 = this;
-	if(options === undefined || options.indexOf("t", 0) > -1) txt_0 = txt_0.split("\t").join(" ");
-	if(options === undefined || options.indexOf("r", 0) > -1) txt_0 = txt_0.split("\r").join(" ");
-	if(options === undefined || options.indexOf("n", 0) > -1) txt_0 = txt_0.split("\n").join(" ");
-	if(options === undefined || options.indexOf("s", 0) > -1) {
-		txt_0 = txt_0.split(" ");
-		for(idx in txt_0) {
-			if(txt_0[idx].length === 0) txt_0.splice(idx, 1);
-		}
-		txt_0 = txt_0.join(" ");
+let checkShowIncludes = function(val) {
+	if(showTitles.includes(val) || val.length > 0) {
+		return true;
+	} else if(val === "" || val.length === 0) {
+		return false;
 	}
-	if(options === undefined || options.indexOf("&apos;", 0) > -1) txt_0 = txt_0.split("&apos;").join("\'");
-	if(options === undefined || options.indexOf("&apos;", 0) > -1) txt_0 = txt_0.split("&amp;").join("&");
-	if(txt_0.indexOf("<") >= 0 && txt_0.indexOf(">") >= 0) {
-		let txt_3 = new Array();
-		let txt_1 = 0;
-		while(txt_0.indexOf(">", txt_1) > txt_1) {
-			cut = txt_1,
-			txt_1 = txt_1 + 1;
-		}
-		txt_1 = cut + 2;
-		while(_loc2_.indexOf("<", _loc3_) > _loc3_) {
-			txt_3.push(txt_0.charAt(txt_1));
-			txt_1 = txt_1 + 1;
-		}
-		txt_0 = txt_3.join("").toString();
+}
+
+String.prototype.stripWhitespace = function() {
+	let txt_0 = String(this);
+	if(txt_0.toUpperCase() === txt_0) {
+		return (txt_0.length > 25 ? `${txt_0.slice(0, 25)} .&nbsp.&nbsp.` : txt_0);
+	} else if(txt_0.toLowerCase() === txt_0) {
+		return (txt_0.length > 31 ? `${txt_0.slice(0, 31)} .&nbsp.&nbsp.` : txt_0);
+	} else {
+		return (txt_0.length > 28 ? `${txt_0.slice(0, 28)} .&nbsp.&nbsp.` : txt_0);
 	}
-	if(txt_0.indexOf("&lt;") >= 0 && txt_0.indexOf("&gt;") >= 0) {
-		txt_3 = new Array(),
-		txt_1 = 0;
-		while(txt_0.indexOf("&gt;", txt_1) > txt_1) {
-			cut = txt_1,
-			txt_1 = txt_1 + 1;
-		}
-		txt_1 = cut + 5;
-		while(txt_0.indexOf("&lt;", txt_1) > txt_1) {
-			txt_3.push(txt_0.charAt(txt_1));
-			txt_1 = txt_1 + 1;
-		}
-		txt_0 = txt_3.join("").toString();
-	}
-	if(txt_0.length >= 27) txt_0 = `${txt_0.slice(0,26)} .&nbsp.&nbsp.`;
-	if(txt_0) return txt_0;
-	return "";
 }
 
 function setWatch(ts) {
@@ -177,7 +156,7 @@ function setWatch(ts) {
 				<span style="color: #666666">${hours}</span>
 				<span style="color: #A6A6A6; margin-left: -4px">:${minutes}</span>
 				<span style="color: rgb(255,255,255,1); margin-left: -4px">:${seconds}</span>
-				<span style="color: #575757">&nbsp${milliseconds}</span>
+				<span style="color: rgb(87,87,87,.5)">&nbsp${milliseconds}</span>
 			`);
 			break;
 		}
@@ -191,24 +170,35 @@ function setWatch(ts) {
 	}
 }
 
-function showMessage(txt) {
+function showMessage(txt, val) {
 	let msg;
-	switch(txt) {
-		case 'closeTab': msg = 'Теперь вкладку можно закрыть.'; break;
-		case 'copy': msg = 'Текст скопирован в буфер обмена'; break;
-		case 'copytext': msg = 'Нажми два раза, чтобы скопировать в буфер обмена'; break;
-		case 'mute': msg = 'Звук приглушен'; break;
-		case 'nocopy': msg = 'Текст не был скопирован'; break;
-		case 'noselect': msg = 'Станция не выбрана'; break;
-		case 'noconnect': msg = 'Ошибка подключения к аудиопотоку. Проверьте состояние интернет-соединения или попробуйте подключиться позже.'; break;
-		case 'notloaded': msg = 'Ошибка загрузки данных о текущем треке'; break;
-		case 'nowplaying': msg = 'Воспроизведение остановлено'; break;
-		case 'restart': msg = 'Воспроизведение перезапущено'; break;
-		case 'unmute': msg = 'Звук возобновлен'; break;
-		default: msg = 'Не понимаю, что ты от меня хочешь';
+	if(txt !== undefined && val === undefined) {
+		switch(txt) {
+			case 'closeTab': msg = 'Теперь вкладку можно закрыть.'; break;
+			case 'copy': msg = 'Текст скопирован в буфер обмена'; break;
+			case 'copytext': msg = 'Нажми два раза, чтобы скопировать в буфер обмена'; break;
+			case 'mute': msg = 'Звук приглушен'; break;
+			case 'nocopy': msg = 'Текст не был скопирован'; break;
+			case 'noselect': msg = 'Станция не выбрана'; break;
+			case 'noconnect': msg = 'Ошибка подключения к аудиопотоку. Проверьте состояние интернет-соединения или попробуйте подключиться позже.'; break;
+			case 'notloaded': msg = 'Ошибка загрузки данных о текущем треке'; break;
+			case 'notplaying': msg = 'В данный момент ничего не играет.';
+			case 'nowplaying': msg = 'Воспроизведение остановлено'; break;
+			case 'restart': msg = 'Воспроизведение перезапущено'; break;
+			case 'unmute': msg = 'Звук возобновлен'; break;
+			case 'volume': msg = `Громкость ${smVolume}%`; break;
+			default: msg = 'Не понимаю, что ты от меня хочешь';
+		}
+		$('.tooltip').html(msg).addClass('active');
+		setTimeout(() => $('.tooltip').removeClass('active'), 5000+String(msg).length*50);
+	} else if(txt !== undefined && val !== undefined) {
+		switch(txt) {
+			case 'volume': msg = `Громкость ${val}%`; break;
+		}
+		$('.tooltip').hasClass('active') ? $('.tooltip').html(msg) : $('.tooltip').html(msg).addClass('active');
+	} else {
+		$('.tooltip').removeClass('active');
 	}
-	$('.tooltip').html(msg).addClass('active');
-	setTimeout(() => $('.tooltip').removeClass('active'), 6000+String(msg).length*50);
 }
 
 function checkLSItems() {
@@ -221,7 +211,7 @@ function checkLSItems() {
 function setLSItem(id) {
 	switch(id) {
 		case 'copyTextTip': localStorage[id] = false; break;
-		case 'currVolume': localStorage[id] = '70'; break;
+		case 'currVolume': localStorage[id] = '75'; break;
 	}
 }
 
@@ -274,8 +264,4 @@ function setLSItem(id) {
 			localStorage.playSource = '';
 		}
 	});
-
 })(jQuery);
-
-
-
